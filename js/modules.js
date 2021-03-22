aba301f41451cf4edbf3ab227c3e9c02ae6262bb
Erik Tejeda <etrq@protonmail.com>
2021-03-22 14:42:46 -0600

    Finish tictactoe cli functionality

--- js/modules.js
+++ js/modules.js
@@ -1,47 +1,18 @@
-/*
-
-let combinations = [
-  [b[0], b[1], b[2]],
-  [b[3], b[4], b[5]],
-  [b[6], b[7], b[8]],
-  [b[0], b[3], b[6]],
-  [b[1], b[4], b[7]],
-  [b[2], b[5], b[8]]
-]
-
-let winner = false
-combinations.forEach(combination => {
-  if(combination == [token, token, token]) return winner = true
-}
-*/
-
 const tictactoe = (function() {
-  let b
-
-  /*
-  const playerProto = {
-    win() {
-      this.name + "won!"
-    }
-  }
-  const Player = (token, name) => {
-    return Object.assign(Object.create(playerProto), {
-      token, name
-    })
-  }
-  */
+  let board
+  let started
+  let over
 
   let players = {}
   let lastPlayer
-  let started = false
   let winner
 
   const createPlayer = function(token, name) {
+    if(over) restartGame(true)
     let tokens = Object.keys(players)
     if(tokens.length == 2) {
       throw("This is a 2 player game")
-    } else if(tokens.length == 1 &&
-              token == tokens[0]) {
+    } else if(token == tokens[0]) {
       throw("Please choose another token")
     }
     players[token] = name
@@ -49,47 +20,60 @@ const tictactoe = (function() {
   const listPlayers = function() {
     return players
   }
+  const restartGame = function(reset_players=false) {
+    console.log("Restarting game...")
+    if(reset_players) players = {}
+    board      = null
+    started    = null
+    over       = null
+    lastPlayer = null
+    winner     = null
+  }
   const startGame = function() {
+    if(over) restartGame()
     let tokens = Object.keys(players)
     if(tokens.length != 2) throw("This is a 2 player game")
-    b = [
+    board = [
       [null, null, null], [null, null, null], [null, null, null]
     ]
     started = true
   }
   const mark = function(token, row, column) {
-    if(!started) {
+    if(!started || over) {
       throw("Please start the game")
     } else if(lastPlayer == token) {
       throw("You can't do that")
-    } else if(b[row][column] !== null) {
+    } else if(board[row][column] !== null) {
       throw("That cell is not empty")
     }
-    b[row][column] = token
-    checkWin(token)
-
+    board[row][column] = token
     lastPlayer = token
-    return b
+
+    checkWin(token)
+    return board
   }
   const checkWin = function(token) {
     let combinations = [
-      [b[0], b[1], b[2]], [b[3], b[4], b[5]], [b[6], b[7], b[8]],
-      [b[0], b[3], b[6]], [b[1], b[4], b[7]], [b[2], b[5], b[8]],
-      [b[0], b[4], b[8]], [b[2], b[4], b[6]]
+      board[0],     board[1],    board[2],
+      [board[0][0], board[1][0], board[2][0]],
+      [board[0][1], board[1][1], board[2][1]],
+      [board[0][2], board[1][2], board[2][2]],
+      [board[0][0], board[1][1], board[2][2]],
+      [board[0][2], board[1][1], board[2][0]]
     ]
-
-    let winner = false
     combinations.forEach(combination => {
-      if(combination == [token, token, token]) {
-        console.log(true)
-        return winner = true
+      if(combination.every((v,i) => v === token)) {
+        over   = true
+        winner = true
       }
     })
-    console.log(winner)
     if(winner) win(token)
   }
   const win = function(token) {
     throw(`${players[token]} won!`)
+  }
+  const debug = function() {
+    console.log([board, started, over, lastPlayer, winner])
   }
 
   return {
@@ -97,6 +81,6 @@ const tictactoe = (function() {
     listPlayers,
     startGame,
     mark,
-    winner
+    debug
   }
 })()
